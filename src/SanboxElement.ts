daecbbacc99a10f3802639ad364c97e2f658e08f
trieu.vo <trieu.vo@insightus.com.au>
2024-05-24 21:31:34 +0700

    fix sandbox

--- src/SanboxElement.ts
+++ src/SanboxElement.ts
@@ -1,6 +1,7 @@
 import { IPageSanbox } from "./interfaces/IPageSandbox";
 import { ISanboxElement } from "./interfaces/ISanboxElement";
 import { ISanboxElementSelector } from "./interfaces/ISanboxElementSelector";
+import { ISandboxScript } from "./interfaces/ISandboxScript";
 import { EventClickCreateSandboxScript } from "./sandbox-scripts/EventClickCreateSandboxScript";
 import { EventDispatchSandboxScript } from "./sandbox-scripts/EventDispatchSandboxScript";
 import { SandboxScriptBlock } from "./SandboxScriptBlock";
@@ -13,14 +14,22 @@ export class SanboxElement implements ISanboxElement {
     this.selector = selector;
   }
   click() {
-    const codeblock = new SandboxScriptBlock();
     const clickEvent = new EventClickCreateSandboxScript();
+    this.dispatchEvent(clickEvent);
+  }
+
+  dispatchEvent(event: ISandboxScript) {
+    const codeblock = new SandboxScriptBlock();
     const selectorScript = this.selector.getScript();
     const dispatchScript = new EventDispatchSandboxScript(
-      clickEvent,
+      event,
       selectorScript
     );
     codeblock.add(dispatchScript);
+    this.executeScript(codeblock);
+  }
+
+  private executeScript(codeblock: SandboxScriptBlock) {
     const executor = this.page.getFunctionExecutor();
     executor.executeScript(codeblock);
   }
